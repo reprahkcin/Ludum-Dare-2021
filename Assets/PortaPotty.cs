c7d416bc8b8c5a576b7d5c71ecdbd8e012b9dedd
Nick Harper <bignickharper@gmail.com>
2021-10-03 08:35:36 -0700

    Map and fence - Bathroom waypoints

--- Assets/PortaPotty.cs
+++ Assets/PortaPotty.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class PortaPotty : MonoBehaviour
 {
+    [SerializeField]
+    private Transform _queueStart;
+    [SerializeField]
+    private Transform _seatedPosition;
+
     public Animator anim;
 
     public void OpenDoor()
     {
         anim.SetTrigger("OpenDoor_Trigger");
 
+    }
+
+    public Transform GetQueueStart()
+    {
+        return _queueStart;
+    }
+
+    public Transform GetSeatedPosition()
+    {
+        return _seatedPosition;
     }
 
 }
