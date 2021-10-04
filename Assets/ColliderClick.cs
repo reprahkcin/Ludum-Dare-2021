b3cb9e1231dc5005938366f2348bba48bf7e779b
Nick Breslin <nickbreslin@gmail.com>
2021-10-04 13:40:25 -0700

    Ferris Wheel repair

--- Assets/ColliderClick.cs
+++ Assets/ColliderClick.cs
@@ -6,20 +6,13 @@ public class ColliderClick : MonoBehaviour
 {
     public Transform target;
 
-    void Update()
-    {
-
-        if(Input.GetMouseButtonDown(1)) {
-            GUIManager.instance.SetActionTarget(target);
-        }
-    }
-
     void OnMouseOver () 
     {
         if(Input.GetMouseButtonDown(0))
             Debug.Log("Left click on this object");
         if(Input.GetMouseButtonDown(1))
             Debug.Log("Right click on this object");
+            GUIManager.instance.SetActionTarget(target);
         if(Input.GetMouseButtonDown(2))
             Debug.Log("Middle click on this object");
     }
